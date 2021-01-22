6538d97e89f1d3dfbbc5627f65148af41ee439e0
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-01-22 15:46:57 +0100

    Microservices in the spring cloud, work with Eureka and API Gateway

---
+++ src/main/java/com/it2go/micro/cma/apigateway/CmaApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.it2go.micro.cma.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmaApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CmaApiGatewayApplication.class, args);
+  }
+}
---
+++ src/test/java/com/it2go/micro/cma/apigateway/CmaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it2go.micro.cma.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmaApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
